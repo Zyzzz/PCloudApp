ea250b5d4601fec3e329be2010c10d0942446dc9
guyu <guyu@imudges.com>
2016-05-10 15:24:49 +0800

    create httpclient

---
+++ src/imu/pcloud/app/utils/HttpClient.java
@@ -0,0 +1,26 @@
+package imu.pcloud.app.utils;
+
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+import com.loopj.android.http.PersistentCookieStore;
+import com.loopj.android.http.RequestParams;
+
+/**
+ * Created by guyu on 2016/5/10.
+ */
+public class HttpClient {
+    private static AsyncHttpClient client = new AsyncHttpClient();
+    private static final String BASE_URL = "http://localhost:8080/PCloudServer/";
+
+    public static void get(String url, RequestParams params, AsyncHttpResponseHandler responseHandler) {
+        client.get(getAbsoluteUrl(url), params, responseHandler);
+    }
+
+    public static void post(String url, RequestParams params, AsyncHttpResponseHandler responseHandler) {
+        client.post(getAbsoluteUrl(url), params, responseHandler);
+    }
+
+    private static String getAbsoluteUrl(String relativeUrl) {
+        return BASE_URL + relativeUrl;
+    }
+}
