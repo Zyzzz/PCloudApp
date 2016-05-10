8e669dd7a2d3a3e7ef0cff0069eafdaa00bca95e
guyu <guyu@imudges.com>
2016-05-10 14:28:17 +0800

    base app create

---
+++ src/imu/pcloud/app/imu/pcloud/app/service/TestService.java
@@ -0,0 +1,16 @@
+package imu.pcloud.app.imu.pcloud.app.service;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+/**
+ * Created by guyu on 2016/5/10.
+ */
+public class TestService extends Service {
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+
+    }
+}
