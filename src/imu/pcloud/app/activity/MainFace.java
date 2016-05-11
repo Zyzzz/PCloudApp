e7300dbd4cf553ff2893602733386f0a6efd1ec6
chenyangyang <chenyangyang@imudges.com>
2016-05-11 12:20:06 +0800

    修改错误

---
+++ src/imu/pcloud/app/activity/MainFace.java
@@ -0,0 +1,27 @@
+package imu.pcloud.app.activity;
+
+import android.app.*;
+import android.os.Bundle;
+import android.os.CancellationSignal;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.view.View.OnClickListener;
+import imu.pcloud.app.R;
+
+/**
+ * Created by acer on 2016/5/8.
+ */
+public class MainFace extends Activity {
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.mainface);
+
+    }
+
+
+
+
+
+}
---
+++ src/imu/pcloud/app/activity/MyActivity.java
@@ -0,0 +1,63 @@
+package imu.pcloud.app.activity;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+import imu.pcloud.app.R;
+
+public class MyActivity extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+        Button error_button = (Button) findViewById(R.id.register);
+        Button login_button = (Button) findViewById(R.id.login);
+        error_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setClass(MyActivity.this, Findpad.class);
+                startActivity(intent);
+            }
+        });
+        login_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setClass(MyActivity.this,MainFace.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.text, menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                break;
+            case R.id.action_refresh:
+                Intent intent1 = new Intent();
+                intent1.setClass(MyActivity.this, Findpad.class);
+                startActivity(intent1);
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+}
