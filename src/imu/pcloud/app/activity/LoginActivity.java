bd8fb377c01b17ec279847b67d1df2d686063ce1
guyu <guyu@imudges.com>
2016-05-11 18:02:10 +0800

    create HttpFragment

--- src/imu/pcloud/app/activity/LoginActivity.java
+++ src/imu/pcloud/app/activity/LoginActivity.java
@@ -7,12 +7,16 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import imu.pcloud.app.R;
+import imu.pcloud.app.model.UserModel;
 
-public class LoginActivity extends HttpActivity {
+public class LoginActivity extends HttpActivity implements View.OnClickListener {
 
     private Button register;
     private Button login;
+    private TextView email;
+    private TextView password;
     /**
      * Called when the activity is first created.
      */
@@ -21,11 +25,31 @@ public class LoginActivity extends HttpActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_layout);
         register = find(R.id.register);
+        login = find(R.id.login);
+        email = find(R.id.email_text);
+        password = find(R.id.password_text);
     }
 
     @Override
     protected void OnSuccess() {
+        UserModel model = getObject(UserModel.class);
+        if(model.getStatus() != 0) {
+            toast(model.getResult());
 
+        }
+        else {
+            toast("登录成功");
+        }
+    }
+
+
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.login:
+                get("login", "email", email.getText(), "password", password.getText());
+        }
     }
 
     /*public boolean onCreateOptionsMenu(Menu menu) {
