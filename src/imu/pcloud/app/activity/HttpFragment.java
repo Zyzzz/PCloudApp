56827b7c9e67db3b123b333e0dbf34f3558a6391
guyu <guyu@imudges.com>
2016-05-11 15:43:39 +0800

    create HttpFragment

---
+++ src/imu/pcloud/app/activity/HttpFragment.java
@@ -0,0 +1,78 @@
+package imu.pcloud.app.activity;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+import imu.pcloud.app.utils.HttpClient;
+import imu.pcloud.app.utils.ViewFinder;
+import org.apache.http.Header;
+
+/**
+ * Created by guyu on 2016/5/11.
+ */
+abstract public class HttpFragment extends Fragment {
+    protected String jsonString = null;
+    protected MyAsyncHttpResponseHandler myHandler = new MyAsyncHttpResponseHandler();
+    protected Gson gson = new GsonBuilder().create();
+    protected SharedPreferences sharedPreferences;
+    protected SharedPreferences.Editor editor;
+    protected ViewFinder finder;
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        sharedPreferences = getActivity().getSharedPreferences("userinfo", getActivity().MODE_PRIVATE);
+        editor = sharedPreferences.edit();
+        finder = new ViewFinder(getActivity());
+    }
+
+    public <V extends View> V find(final int id) {
+        return finder.find(id);
+    }
+    void get(String url, Object... prams) {
+        RequestParams requestParams = new RequestParams();
+        for (int i = 0; i < prams.length; i += 2) {
+            requestParams.put((String) prams[i],
+                    prams[i + 1].getClass().cast(prams[i + 1]));
+        }
+        HttpClient.get(url, requestParams, myHandler);
+    }
+
+    protected <T> T getObject(Class<T> t) {
+        return gson.fromJson(jsonString, t);
+    }
+
+    abstract protected void OnSuccess();
+
+    protected void toast(String string) {
+        Toast.makeText(getActivity(), string, Toast.LENGTH_SHORT).show();
+    }
+
+    protected void setCookie(String cookie){
+        editor.putString("cookie", cookie);
+        editor.commit();
+    }
+
+    protected String getCookie() {
+        return sharedPreferences.getString("cookie", "");
+    }
+
+    class MyAsyncHttpResponseHandler extends AsyncHttpResponseHandler {
+        @Override
+        public void onSuccess(int i, Header[] headers, byte[] bytes) {
+            jsonString = new String(bytes);
+            OnSuccess();
+        }
+        @Override
+        public void onFailure(int i, Header[] headers, byte[] bytes, Throwable throwable) {
+
+        }
+    }
+}
