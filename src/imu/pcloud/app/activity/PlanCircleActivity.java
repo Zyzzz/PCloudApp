fda0307fb142b76c7c4ab8017e8e62225a6d2b4d
zyzzz <zhouyan@imudges.com>
2016-05-17 16:12:23 +0800

    PlanCircleActivity

---
+++ src/imu/pcloud/app/activity/PlanCircleActivity.java
@@ -0,0 +1,20 @@
+package imu.pcloud.app.activity;
+
+import android.os.Bundle;
+
+/**
+ * Created by Administrator on 2016/5/17.
+ */
+public class PlanCircleActivity extends HttpActivity{
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected void onSuccess() {
+
+    }
+}
