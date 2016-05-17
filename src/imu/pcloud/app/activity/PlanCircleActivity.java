df2df5ceec9be7603ab36f581c63bbf975ffd84f
zyzzz <zhouyan@imudges.com>
2016-05-17 17:33:52 +0800

    两个界面通信完成

--- src/imu/pcloud/app/activity/PlanCircleActivity.java
+++ src/imu/pcloud/app/activity/PlanCircleActivity.java
@@ -1,5 +1,6 @@
 package imu.pcloud.app.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 /**
@@ -12,7 +13,10 @@ public class PlanCircleActivity extends HttpActivity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-
+        //getActionBar().setDisplayHomeAsUpEnabled(true);
+        Intent intent_accept = getIntent();
+        Bundle bundle = intent_accept.getExtras();
+        int planCircleID = bundle.getInt("planCircleID");
     }
 
     @Override
