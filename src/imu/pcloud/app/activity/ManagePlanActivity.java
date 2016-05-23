0b270933ff8ee53c82db1378d7409c9be8ebeee6
chenyangyang <chenyangyang@imudges.com>
2016-05-23 20:22:58 +0800

    123

---
+++ src/imu/pcloud/app/activity/ManagePlanActivity.java
@@ -0,0 +1,17 @@
+package imu.pcloud.app.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import imu.pcloud.app.R;
+
+/**
+ * Created by acer on 2016/5/23.
+ */
+public class ManagePlanActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.manageplan_layout);
+    }
+}
