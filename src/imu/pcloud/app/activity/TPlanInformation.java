898b4119bc42c0765efdbfc68584d2ec1013d54e
chenyangyang <chenyangyang@imudges.com>
2016-06-02 18:42:56 +0800

    123

--- src/imu/pcloud/app/activity/TPlanInformation.java
+++ src/imu/pcloud/app/activity/TPlanInformation.java
@@ -1,19 +1,26 @@
 package imu.pcloud.app.activity;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import imu.pcloud.app.R;
 
 /**
  * Created by acer on 2016/6/1.
  */
-public class TPlanInformation extends Activity {
+public class TPlanInformation extends HttpActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.tplancontent);
+    }
+
+    @Override
+    protected void onSuccess() {
+
     }
 
     @Override
@@ -22,4 +29,18 @@ public class TPlanInformation extends Activity {
         getMenuInflater().inflate(R.menu.team, menu);
         return super.onCreateOptionsMenu(menu);
     }
+
+    @Override
+    public boolean onMenuItemSelected(int featureId, MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.lookmember:
+                startActivity(TeamMemberActivity.class);
+                break;
+            case R.id.updateplan:
+                break;
+        }
+        return super.onMenuItemSelected(featureId, item);
+    }
+
+
 }
--- src/imu/pcloud/app/fragment/TeamFragment.java
+++ src/imu/pcloud/app/fragment/TeamFragment.java
@@ -1,17 +1,33 @@
 package imu.pcloud.app.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.*;
+import android.widget.AdapterView;
+import android.widget.ListView;
 import android.widget.TextView;
 import imu.pcloud.app.R;
+import imu.pcloud.app.utils.MyAdspter;
 
 /**
  * Created by acer on 2016/5/11.
  */
 public class TeamFragment extends HttpFragment {
+    private ListView listview;
+    MyAdspter myAdspter;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         initActionBar();
+        listview= (ListView) getActivity().findViewById(R.id.teammember_list);
+        listview.setAdapter(myAdspter);
+        listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent=new Intent();
+
+
+            }
+        });
         return inflater.inflate(R.layout.team_layout, container, false);
     }
 
