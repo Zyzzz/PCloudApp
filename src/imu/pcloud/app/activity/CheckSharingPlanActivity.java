9efe46abb5b690470e745ab29c9840b4d7a00407
zyzzz <zhouyan@imudges.com>
2016-06-02 16:21:54 +0800

    CheckSharingPlanActivityAndErrorCode

--- src/imu/pcloud/app/activity/CheckSharingPlanActivity.java
+++ src/imu/pcloud/app/activity/CheckSharingPlanActivity.java
@@ -4,21 +4,30 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ListView;
 import imu.pcloud.app.R;
 
 /**
  * Created by Administrator on 2016/6/1.
  */
 public class CheckSharingPlanActivity extends HttpActivity{
+
+    ListView planItemListView;
+    ListView planComment;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.check_sharing_plan);
+        planItemListView = find(R.id.plan_informationf);
+        planComment = find(R.id.comment_listview);
         Intent intent_accept = getIntent();
         Bundle bundle = intent_accept.getExtras();
         // getActionBar().setDisplayHomeAsUpEnabled(true);
         String planName = bundle.getString("planName");
+        String planContext = bundle.getString("planContext");
+        int planId = bundle.getInt("PlanId");
         setActionBar(""+planName);
+        get("","");
     }
 
     @Override
@@ -36,6 +45,10 @@ public class CheckSharingPlanActivity extends HttpActivity{
         switch (item.getItemId()) {
             case android.R.id.home:
                 finish();
+                break;
+            case R.id.downloan_plan:
+                break;
+            case R.id.comment_plan:
                 break;
         }
         return super.onOptionsItemSelected(item);
