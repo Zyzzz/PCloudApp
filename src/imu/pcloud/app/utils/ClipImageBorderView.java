5e0831ab6c7e9b11b3c72b39f3bba51dbdeebde8
zyzzz <zhouyan@imudges.com>
2016-06-07 17:06:23 +0800

    ClipImageBorder

---
+++ src/imu/pcloud/app/utils/ClipImageBorderView.java
@@ -0,0 +1,68 @@
+package imu.pcloud.app.utils;
+
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Paint.Style;
+import android.util.AttributeSet;
+import android.util.TypedValue;
+import android.view.View;
+
+public class ClipImageBorderView extends View
+{
+	/**
+	 * ˮƽ������View�ı߾�
+	 */
+	private int mHorizontalPadding;
+	/**
+	 * �߿�Ŀ�� ��λdp
+	 */
+	private int mBorderWidth = 2;
+
+	private Paint mPaint;
+
+	public ClipImageBorderView(Context context)
+	{
+		this(context, null);
+	}
+
+	public ClipImageBorderView(Context context, AttributeSet attrs)
+	{
+		this(context, attrs, 0);
+	}
+
+	public ClipImageBorderView(Context context, AttributeSet attrs, int defStyle)
+	{
+		super(context, attrs, defStyle);
+	
+		mBorderWidth = (int) TypedValue.applyDimension(
+				TypedValue.COMPLEX_UNIT_DIP, mBorderWidth, getResources()
+						.getDisplayMetrics());
+		mPaint = new Paint();
+		mPaint.setAntiAlias(true);
+	}
+
+	@Override
+	protected void onDraw(Canvas canvas)
+	{
+		super.onDraw(canvas);
+		// ���Ʊ߿�
+		mPaint.setColor(Color.parseColor("#FFFFFF"));
+		mPaint.setStrokeWidth(mBorderWidth);
+		mPaint.setStyle(Style.STROKE);
+		//���α߿�
+//		canvas.drawRect(mHorizontalPadding, mVerticalPadding, getWidth()- mHorizontalPadding, getHeight() - mVerticalPadding, mPaint);
+		//Բ�α߿�
+		canvas.drawCircle( getWidth()/2, getHeight()/2, getWidth()/2-mHorizontalPadding, mPaint);
+
+	}
+
+	public void setHorizontalPadding(int mHorizontalPadding)
+	{
+		this.mHorizontalPadding = mHorizontalPadding;
+		
+	}
+
+}
